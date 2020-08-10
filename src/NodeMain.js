d00d1698f1877e3c1b4b4b0b1177b7cb272894b6
wcourtney31415 <wcourtney31415@gmail.com>
2020-08-10 00:58:47 -0400

    Added functions for handling various http requests. This will need a lot of refactoring soon.

--- src/NodeMain.js
+++ src/NodeMain.js
@@ -1,12 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 const express = require('express')
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express()
 
-const httpAddress = 'localhost'
+const host = 'localhost'
 const httpPort = 80
-const link = `http://${httpAddress}:${httpPort}`
+const address = `http://${host}:${httpPort}`
 
 const mongoAddress = 'localhost'
 const mongoPort = 27017
@@ -14,7 +15,18 @@ const mongoUrl = `mongodb://${mongoAddress}:${mongoPort}/`;
 const dbName = "MyDb"
 const myColName = "users"
 
-app.get('/people', (req, res) => {
+const linkPeople = '/people';
+const linkTest = '/test';
+const linkThis = '/this';
+const links = [linkPeople, linkTest, linkThis]
+
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get(linkPeople, (req, res) => {
   MongoClient.connect(mongoUrl, function(err, db) {
     if (err) throw err;
     const dbo = db.db(dbName);
@@ -29,10 +41,10 @@ app.get('/people', (req, res) => {
       db.close();
     });
   });
-  console.log(`${link}/people accessed.`);
+  console.log(`${address}/people accessed.`);
 })
 
-app.get('/test', (req, res) => {
+app.get(linkTest, (req, res) => {
   const myJson = {
     _id: "5f27882f5d389500061a6deb",
     firstName: "Celeste",
@@ -46,18 +58,68 @@ app.get('/test', (req, res) => {
     country: "Mongolia"
   };
   res.json(myJson);
-  console.log(`${link}/test accessed.`);
+  console.log(`${address}/test accessed.`);
 })
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
-  console.log(`${link}/test accessed.`);
+  console.log(`${address}/test accessed.`);
 })
 
+const query = {
+  lastName: "Green"
+};
 
+function dbQuery(dbUrl, dbName, colName, query) {
+  MongoClient.connect(dbUrl, function(err, connection) {
+    if (err) throw err;
+    const database = connection.db(dbName);
+    const collection = database.collection(colName);
+    const queryResults = collection.find(query)
+    queryResults.toArray(function(err, result) {
+      if (err) throw err;
+      console.log("Within the function: ".concat(result));
+      return result;
+      database.close();
+    });
+  });
+}
+
+app.post(linkThis, (request, response) => {
+  body = request.body
+  token = parseInt(body.token)
+  var result;
+  if (token === 5) {
+    MongoClient.connect(mongoUrl, function(err, db) {
+      if (err) throw err;
+      const dbo = db.db(dbName);
+      const collection = dbo.collection(myColName);
+      const query = {
+        lastName: "Green"
+      };
+      const queryResults = collection.find(query)
+      queryResults.toArray(function(err, result) {
+        if (err) throw err;
+        response.json(result);
+        db.close();
+      });
+    });
+  } else {
+    response.json("[]");
+  }
+});
 
 //Keeps the server listening
 app.listen(httpPort, () => {
-  const msg = `Server Started: ${link}`
-  console.log(msg)
+  startupMessage();
 })
+
+function startupMessage() {
+  const msg = `Server Started: ${address}`
+  console.log(msg)
+  links.forEach(path => {
+    fullPath = address + path;
+    console.log(fullPath);
+  });
+  console.log("")
+}
