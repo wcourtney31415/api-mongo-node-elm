36f70bfa921094ef00dff9f0a53994351f4f1c1d
wcourtney31415 <wcourtney31415@gmail.com>
2020-08-12 11:46:27 -0400

    Refactored the NodeMain.js file. Removed no longer used functions.

--- src/NodeMain.js
+++ src/NodeMain.js
@@ -4,81 +4,31 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express()
+const jsonParser = bodyParser.json()
 
+//Webserver
 const host = 'localhost'
 const httpPort = 80
 const address = `http://${host}:${httpPort}`
 
+//MongoDB
 const mongoAddress = 'localhost'
 const mongoPort = 27017
 const mongoUrl = `mongodb://${mongoAddress}:${mongoPort}/`;
 const dbName = "MyDb"
 const myColName = "users"
 
+//Links
 const linkGetPeople = '/getShowPeople';
 const linkNonDB = '/nonDatabaseRequest';
 const linkPostPeople = '/postShowPeople';
 const links = [linkGetPeople, linkNonDB, linkPostPeople]
-
-
-// app.use(bodyParser.urlencoded({
-//   extended: false
-// }))
-//
-// app.use(bodyParser.json())
-
-var jsonParser = bodyParser.json()
-
-
-app.get(linkGetPeople, (request, response) => {
-  MongoClient.connect(mongoUrl, function(err, db) {
-    if (err) throw err;
-    const dbo = db.db(dbName);
-    const collection = dbo.collection(myColName);
-    const query = {
-      lastName: "Green"
-    };
-    const queryResults = collection.find(query)
-    queryResults.toArray(function(err, result) {
-      if (err) throw err;
-      response.send(result);
-      db.close();
-    });
-  });
-  logPageServed(linkGetPeople);
-})
-
-app.get(linkNonDB, (request, response) => {
-  const jsonExample = {
-    _id: "5f27882f5d389500061a6deb",
-    firstName: "Celeste",
-    lastName: "Green",
-    phoneNumber: "(649) 366-4307",
-    email: "interdum.libero.dui@sitamet.com",
-    birthdate: "02-03-15",
-    streetAddress: "P.O. Box 645, 3777 Vitae, Street",
-    city: "Castel Maggiore",
-    zip: "39417",
-    country: "Mongolia"
-  };
-  response.json(jsonExample);
-  logPageServed(linkNonDB);
-})
 
 app.get('/', (request, response) => {
   const homepage = getFile('index.html');
   response.sendFile(homepage);
   logPageServed("");
 })
-
-function getFile(fileName) {
-  return path.join(__dirname + '/' + fileName)
-}
-
-function logPageServed(path) {
-  const fullAddress = address + path
-  console.log(`Served up: ${fullAddress}`);
-}
 
 app.post(linkPostPeople, jsonParser, (request, response) => {
   body = request.body
@@ -87,14 +37,12 @@ app.post(linkPostPeople, jsonParser, (request, response) => {
     const dbo = db.db(dbName);
     const collection = dbo.collection(myColName);
     const providedLastName = body.lastName;
-    console.log("Request: " + body);
     const query = {
       lastName: providedLastName
     };
     const queryResults = collection.find(query)
     queryResults.toArray(function(err, result) {
       if (err) throw err;
-      console.log(`Response: ${result}`);
       response.json(result);
       db.close();
     });
@@ -102,10 +50,13 @@ app.post(linkPostPeople, jsonParser, (request, response) => {
   logPageServed(linkPostPeople);
 });
 
-//Keeps the server listening
 app.listen(httpPort, () => {
   startupMessage();
 })
+
+function getFile(fileName) {
+  return path.join(__dirname + '/' + fileName)
+}
 
 function startupMessage() {
   const msg = `Server Started: ${address}`
@@ -115,4 +66,9 @@ function startupMessage() {
     console.log(fullPath);
   });
   console.log("")
+}
+
+function logPageServed(path) {
+  const fullAddress = address + path
+  console.log(`Served up: ${fullAddress}`);
 }
