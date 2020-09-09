5a09f28eaa87e8676cbd8ff598596521c23f64ab
wcourtney31415 <wcourtney31415@gmail.com>
2020-09-09 09:25:06 -0400

    Replaced lamda's in nprSignUp with actual functions to help with clarity.

--- src/nodejs/nprSignUp.js
+++ src/nodejs/nprSignUp.js
@@ -6,22 +6,31 @@ const {
 const User = require('./schemas');
 
 
-app.post('/signup', jsonParser, (req, res) => {
-  const body = req.body;
+
+app.post('/signup', jsonParser, signUp);
+
+function signUp(request, response) {
+  const body = request.body;
   console.log(body);
   let newUser = new User();
-  newUser.name = body.name
-  newUser.email = body.email
+  newUser.name = body.name;
+  newUser.email = body.email;
   newUser.setPassword(body.password);
-  newUser.save((err, User) => {
-    if (err) {
-      return res.status(400).send({
-        message: "Failed to add user."
-      });
-    } else {
-      return res.status(201).send({
-        message: "User added successfully."
-      });
-    }
-  });
-});
+  newUser.save(addUser(response));
+}
+
+function addUser(response, err, User) {
+  var status;
+  var message;
+  if (err) {
+    status = 400;
+    message = "Failed to add user.";
+  } else {
+    status = 201;
+    message = "User added successfully.";
+  }
+  const messageObj = {
+    message: message
+  };
+  return response.status(status).send(messageObj);
+}
