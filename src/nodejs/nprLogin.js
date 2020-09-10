b2134961065894a426f6e491891785f8bbc1a0be
wcourtney31415 <wcourtney31415@gmail.com>
2020-09-10 01:38:54 -0400

    Continued to refactor the nprLogin script to make it much more readable.

--- src/nodejs/nprLogin.js
+++ src/nodejs/nprLogin.js
@@ -10,6 +10,22 @@ const moment = require('moment');
 
 const User = require('./schemas');
 
+function respond(res, status, message) {
+  return res.status(status).send({
+    message: message
+  });
+}
+
+function addSession(user) {
+  const sessionId = crypto.randomBytes(16).toString('hex');
+  const exp = moment().add(180, 'days').calendar();
+  const sessionData = {
+    sessionId: sessionId,
+    exp: exp
+  };
+  user.sessions.push(sessionData);
+  user.save();
+}
 
 
 function login(req, res) {
@@ -17,28 +33,17 @@ function login(req, res) {
     email: req.body.email
   };
   User.findOne(query, (err, user) => {
-    if (user === null) {
-      status = 400;
-      message = "User not found.";
-    } else {
-      if (user.validatePassword(req.body.password)) {
-        const sessionId = crypto.randomBytes(16).toString('hex');
-        const exp = moment().add(180, 'days').calendar();
-        const sessionData = {
-          sessionId: sessionId,
-          exp: exp
-        };
-        user.sessions.push(sessionData);
-        user.save();
-        status = 201;
-        message = "User logged in.";
+    if (user) {
+      const password = req.body.password;
+      const passIsCorrect = user.validatePassword(password);
+      if (passIsCorrect) {
+        addSession(user);
+        respond(res, 201, "User logged in.");
       } else {
-        status = 400;
-        message = "Wrong Password.";
+        respond(res, 400, "Wrong Password.");
       }
-      return res.status(status).send({
-        message: message
-      });
+    } else {
+      respond(res, 400, "User not found.");
     }
   });
 }
