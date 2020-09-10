d8b9cd8971063bf87ccedf3bd9ba84496ad5c223
wcourtney31415 <wcourtney31415@gmail.com>
2020-09-10 01:10:26 -0400

    Discovered that the previous commit had some major flaws. I've reverted much of it's changes and then done further refactoring trying to make the code a bit cleaner.

--- src/nodejs/nprLogin.js
+++ src/nodejs/nprLogin.js
@@ -12,42 +12,35 @@ const User = require('./schemas');
 
 
 
-function loginWithEmail(req, err, user) {
-  const password = req.body.password;
-  var message;
-  var status;
-  if (user === null) {
-    message = "User not found.";
-    status = 400;
-  } else {
-    const passwordIsCorrect = user.validatePassword(password);
-    if (passwordIsCorrect) {
-      const sessionId = crypto.randomBytes(16).toString('hex');
-      const expiration = moment().add(180, 'days').calendar();
-      const sessionData = {
-        sessionId: sessionId,
-        exp: expiration
-      };
-      user.sessions.push(sessionData);
-      user.save();
-      message = "User logged in.";
-      status = 201;
-    } else {
-      message = "Wrong Password";
-      status = 400;
-    }
-  }
-  const messageObj = {
-    message: message
-  };
-  return res.status(status).send(messageObj);
-}
-
 function login(req, res) {
   const query = {
     email: req.body.email
   };
-  User.findOne(query, loginWithEmail(req));
+  User.findOne(query, (err, user) => {
+    if (user === null) {
+      status = 400;
+      message = "User not found.";
+    } else {
+      if (user.validatePassword(req.body.password)) {
+        const sessionId = crypto.randomBytes(16).toString('hex');
+        const exp = moment().add(180, 'days').calendar();
+        const sessionData = {
+          sessionId: sessionId,
+          exp: exp
+        };
+        user.sessions.push(sessionData);
+        user.save();
+        status = 201;
+        message = "User logged in.";
+      } else {
+        status = 400;
+        message = "Wrong Password.";
+      }
+      return res.status(status).send({
+        message: message
+      });
+    }
+  });
 }
 
 app.post('/login', jsonParser, login);
